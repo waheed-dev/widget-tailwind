5b3c913b2824b79d46fd544ba5d6091d26599d17
abdul waheed <a.waheed3738@gmail.com>
2022-03-12 17:26:51 +0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,82 +4,83 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      <div>
+          <Head>
+              <title>
+                  Tailwind css widget
+              </title>
+          </Head>
+     <div className={'mt-10 px-3 py-4 lg:py-12 lg:px-3 lg:justify-center lg:items-center mx-auto border border-gray-300 rounded-2xl shadow-xl w-[340px] lg:w-[600px]'}>
+         <div className="w-full px-2 py-1.5 lg:py-3.5 lg:px-3 justify-center text-center bg-gray-300 shadow-inner shadow-slate-500 rounded-full text-sm"
+         >
+             <div
+                 className="flex flex-row transform shadow-slate-700 space-x-1 lg:space-x-2 font-semibold lg:font-bold text-xs"
+             >
+                 <div className="-skew-x-12">
+                     <p
+                         className="py-5 px-1 w-[50px] text-[10px] lg:w-[110px] lg:py-10 lg:text-[13px] text-white bg-[#414141] rounded-tl-[50px] rounded-bl-[80px] hover:shadow-inner hover:shadow-gray-600 justify-center text-center"
+                     >
+                         Claim Filed
+                     </p>
+                 </div>
+                 <div className="-skew-x-6">
+                     <p
+                         className="py-3 px-1 w-[60px] text-[10px] lg:w-[100px] lg:py-8 lg:text-[13px] text-white bg-[#414141] transform -skew-x-6 hover:shadow-inner hover:shadow-gray-600"
+                     >
+                         Claim under review
+                     </p>
+                 </div>
+                 <div className="-skew-x-12">
+                     <p
+                         className="py-5 w-[66px] text-[10px] lg:w-[100px] lg:py-8 lg:text-[13px] text-white bg-[#414141] hover:shadow-inner hover:shadow-gray-600 flex-shrink"
+                     >
+                         Adjudication in progress
+                     </p>
+                 </div>
+                 <div className="-skew-x-12">
+                     <p
+                         className="py-5 px-1 w-[58px] text-[10px] lg:w-[100px] lg:py-8 lg:text-[13px] text-white bg-[#fb4e0a] shadow-inner shadow-gray-600"
+                     >
+                         Claim approved
+                     </p>
+                 </div>
+                 <div className="-skew-x-12">
+                     <p
+                         className="py-5 px-1 w-[50px] text-[10px] lg:w-[110px] lg:py-10 lg:text-[13px] text-white  bg-[#808080] rounded-tr-[70px] rounded-br-[45px]"
+                     >
+                         Claim paid
+                     </p>
+                 </div>
+             </div>
+         </div>
+          <form
+              className=" flex items-center justify-center gap-6 lg:gap-10 mt-2 lg:mt-4 font-semibold  lg:font-bold"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+              <div className="flex flex-row gap-1 lg:gap-3 items-center">
+                  <input type="radio" className="accent-[#fb4e0a]" id="Absence" name="refund" value="Absence" />
+                      <label htmlFor="Absence" className="text-xs lg:text-base">Absence</label>
+              </div>
+              <div className="flex flex-row gap-1 lg:gap-3 items-center">
+                  <input type="radio" className="accent-[#fb4e0a]" id="std" name="refund" value="std" />
+                      <label htmlFor="std" className="text-xs lg:text-base">STD</label>
+              </div>
+              <div className="flex flex-row gap-1 lg:gap-3 items-center">
+                  <input type="radio" className="accent-[#fb4e0a]" id="ltd" name="refund" value="ltd" />
+                      <label htmlFor="ltd" className="text-xs lg:text-base">LTD</label>
+              </div>
+              <div className="flex flex-row gap-1 lg:gap-3 items-center">
+                  <input type="radio" className="accent-[#fb4e0a]" id="cl" name="refund" value="cl" />
+                      <label htmlFor="cl" className="text-xs lg:text-base">Cl</label>
+              </div>
+              <div className="flex flex-row gap-1 lg:gap-3 items-center">
+                  <input type="radio" className="accent-[#fb4e0a]" id="hi" name="refund" value="hi" />
+                      <label htmlFor="hi" className="text-xs lg:text-base">HI</label>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+              </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+          </form>
+     </div>
+      </div>
   )
 }
 
